e9bb599c2ffb8b19ccd0b725cd07320cccb09ec2
Adrizen89 <64806520+Adrizen89@users.noreply.github.com>
2020-09-11 08:12:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,100 +1,46 @@
-//Lien avec l'API
+fetch("http://localhost:3000/api/furniture/")
+.then(response => response.json())
+.then(response =>{
+    console.log(response)
+    response.forEach(element => {
+        let main = document.getElementById('main');
 
-/*const { request } = require("express");
+        let produitContainer = document.createElement('article');
+        main.append(produitContainer);
 
-getAllFurniture = () => {
-    return new Promise((resolve) =>{
-        let request = new XMLHttpRequest() {
-            if (
-                this.readyState == XMLHttpRequest.DONE &&
-                this.status >=200 &&
-                this.status < 400
-            ) {
-                resolve(JSON.parse(this.responseText));
-                console.log("Connecté");
-            } else {
-            }
-        };
-        request.open("GET", "http://localhost:3000/api/furniture/" + idfurniture);
-        request.send();
+        let img = document.createElement('img');
+        img.src = element.imageUrl;
+        produitContainer.append(img);
+
+        let nameProduit = document.createElement('h2');
+        nameProduit.textContent = element.name 
+        produitContainer.append(nameProduit);
+
+        let prixProduit = document.createElement('p');
+        prixProduit.textContent = element.price / 1000 +"0€"
+        produitContainer.append(prixProduit);
+
+        let savoirPlus = document.createElement('a');
+        savoirPlus.textContent = "En savoir plus "
+        savoirPlus.href = "produit.html?id"+element._id
+        produitContainer.append(savoirPlus);
     });
-};
+});
 
-async function furniture(){
-    const furniture = await getAllFurniture();
+/*let idFurniture = "";
+async function detailFurniture() {
+    idFurniture = location.search;
+    const detailFurniture = await element();
+
+    let detailProduit = document.getElementById('detailProduit');
+
+    let articleContainer = document.createElement('article');
+    detailProduit.append(articleContainer);
+
+    let imgTrue = document.createElement('img');
+    imgTrue.src = detailFurniture.imageUrl;
+    articleContainer.append(imgTrue);
 }*/
 
 
-let title = document.createElement('h1');
-
-//img
-let img1 = document.createElement('img');
-let img2 = document.createElement('img');
-let img3 = document.createElement('img');
-//Produits
-let listeProduit = document.createElement('section');
-let produit1 = document.createElement('article');
-let produit2 = document.createElement('article');
-let produit3 = document.createElement('article');
-let nameProduit1 = document.createElement('h2');
-let nameProduit2 = document.createElement('h2');
-let nameProduit3 = document.createElement('h2');
-//Prix
-let prix1 = document.createElement('p');
-let prix2 = document.createElement('p');
-let prix3 = document.createElement('p');
-//En savoir plus
-let savoirPlus1 = document.createElement('a');
-let savoirPlus2 = document.createElement('a');
-let savoirPlus3 = document.createElement('a');
-
-title.textContent = ('Orinoco');
-//Texte Produits
-nameProduit1.textContent = ('Table en Chêne');
-nameProduit2.textContent = ('Table');
-nameProduit3.textContent = ('Chaise en Chêne');
-//Texte Prix
-prix1.textContent = ('99€');
-prix2.textContent = ('119€');
-prix3.textContent = ('149€');
-//Texte En Savoir Plus
-savoirPlus1.textContent = ('En savoir plus...');
-savoirPlus2.textContent = ('En savoir plus...');
-savoirPlus3.textContent = ('En savoir plus...');
-
-//Ajout Atttribut
-savoirPlus1.setAttribute("href", "produit.html");
-savoirPlus2.setAttribute("href", "produit.html");
-savoirPlus3.setAttribute("href", "produit.html");
-
-img1.setAttribute("class", "img1");
-img2.setAttribute("class", "img2");
-img3.setAttribute("class", "img3");
-
-//img
-img1.src = "images/oak_1.jpg";
-img2.src = "images/oak_2.jpg";
-img3.src = "images/oak_3.jpg";
-
-
-header.append(title);
-//Incorporation Produits
-main.append(listeProduit);
-listeProduit.append(produit1, produit2, produit3);
-
-produit1.append(img1);
-produit2.append(img2);
-produit3.append(img3);
-
-produit1.append(nameProduit1);
-produit2.append(nameProduit2);
-produit3.append(nameProduit3);
-//Incorporation Prix
-produit1.append(prix1);
-produit2.append(prix2);
-produit3.append(prix3);
-//Incorporation En Savoir Plus
-produit1.append(savoirPlus1);
-produit2.append(savoirPlus2);
-produit3.append(savoirPlus3);
 
