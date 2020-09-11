8bf6898b0c42d87cee28d6f53f8d22f1981f8046
Adrizen89 <64806520+Adrizen89@users.noreply.github.com>
2020-09-11 15:19:45 +0200

    Update produit.js

--- produit.js
+++ produit.js
@@ -6,45 +6,40 @@ fetch(maRequete)
     console.log(response)
     response.forEach(element => {
 
-        
         let detailProduit = document.getElementById('detailProduit');
 
-        let container = document.createElement('article');
-        detailProduit.append(container);
+                let container = document.createElement('article');
+                detailProduit.append(container);
 
-        let img = document.createElement('img');
-        img.src = element.imageUrl;
-        container.append(img);
+                let img = document.createElement('img');
+                img.src = element.imageUrl;
+                container.append(img);
 
-        let contentDescription = document.createElement('p');
-        contentDescription.textContent = element.description;
-        contentDescription.setAttribute("class", "content");
-        container.append(contentDescription);
+                let contentDescription = document.createElement('p');
+                contentDescription.textContent = element.description;
+                contentDescription.setAttribute("class", "content");
+                container.append(contentDescription);
 
-        let divPrecision = document.createElement('div');
-        divPrecision.setAttribute("class", "precision");
-        container.append(divPrecision);
+                let divPrecision = document.createElement('div');
+                divPrecision.setAttribute("class", "precision");
+                container.append(divPrecision);
 
-        let titleProduit = document.createElement('h2');
-        titleProduit.textContent = element.name;
-        divPrecision.append(titleProduit);
+                let titleProduit = document.createElement('h2');
+                titleProduit.textContent = element.name;
+                divPrecision.append(titleProduit);
 
-        let prixProduit = document.createElement('p');
-        prixProduit.textContent = element.price / 1000 + "0€" ;
-        prixProduit.setAttribute("class", "prix");
-        divPrecision.append(prixProduit);
+                let prixProduit = document.createElement('p');
+                prixProduit.textContent = element.price / 1000 + "0€" ;
+                prixProduit.setAttribute("class", "prix");
+                divPrecision.append(prixProduit);
 
-        let formulaire = document.createElement('select');
-        let option = document.createElement('option');
-        formulaire.setAttribute("id", "choixOption");
-        option.textContent = element.varnish;
-        divPrecision.append(formulaire);
-        formulaire.append(option);
 
-        let btnAjoutPanier = document.createElement('button');
-        btnAjoutPanier.textContent = "Ajouter au panier !";
-        btnAjoutPanier.setAttribute("type", "submit");
-        btnAjoutPanier.setAttribute("class", "btn_ajoutPanier");
-        divPrecision.append(btnAjoutPanier);
-});
+                let btnAjoutPanier = document.createElement('button');
+                btnAjoutPanier.textContent = "Ajouter au panier !";
+                btnAjoutPanier.setAttribute("type", "submit");
+                btnAjoutPanier.setAttribute("class", "btn_ajoutPanier");
+                divPrecision.append(btnAjoutPanier);
+
+                
+        });
 });
