af0ea3715c526e62600f19556ee1e66d30d94c98
Adrizen89 <64806520+Adrizen89@users.noreply.github.com>
2020-09-11 12:33:47 +0200

    Update produit.js

--- produit.js
+++ produit.js
@@ -1,8 +1,11 @@
-fetch("http://localhost:3000/api/furniture/")
+var maRequete = new Request('http://localhost:3000/api/furniture/');
+
+fetch(maRequete)
 .then(response => response.json())
 .then(response =>{
     console.log(response)
     response.forEach(element => {
+
         
         let detailProduit = document.getElementById('detailProduit');
 
@@ -10,10 +13,38 @@ fetch("http://localhost:3000/api/furniture/")
         detailProduit.append(container);
 
         let img = document.createElement('img');
+        img.src = element.imageUrl;
+        container.append(img);
+
         let contentDescription = document.createElement('p');
+        contentDescription.textContent = element.description;
+        contentDescription.setAttribute("class", "content");
+        container.append(contentDescription);
+
+        let divPrecision = document.createElement('div');
+        divPrecision.setAttribute("class", "precision");
+        container.append(divPrecision);
+
         let titleProduit = document.createElement('h2');
+        titleProduit.textContent = element.name;
+        divPrecision.append(titleProduit);
 
         let prixProduit = document.createElement('p');
-        
+        prixProduit.textContent = element.price / 1000 + "0€" ;
+        prixProduit.setAttribute("class", "prix");
+        divPrecision.append(prixProduit);
+
+        let formulaire = document.createElement('select');
+        let option = document.createElement('option');
+        formulaire.setAttribute("id", "choixOption");
+        option.textContent = element.varnish;
+        divPrecision.append(formulaire);
+        formulaire.append(option);
+
+        let btnAjoutPanier = document.createElement('button');
+        btnAjoutPanier.textContent = "Ajouter au panier !";
+        btnAjoutPanier.setAttribute("type", "submit");
+        btnAjoutPanier.setAttribute("class", "btn_ajoutPanier");
+        divPrecision.append(btnAjoutPanier);
 });
 });
