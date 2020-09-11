d6bef44fc3c9e52975fda63354572982d47d8914
Adrizen89 <64806520+Adrizen89@users.noreply.github.com>
2020-09-11 08:38:25 +0200

    Update produit.js

--- produit.js
+++ produit.js
@@ -7,5 +7,13 @@ fetch("http://localhost:3000/api/furniture/")
         let detailProduit = document.getElementById('detailProduit');
 
         let container = document.createElement('article');
+        detailProduit.append(container);
+
+        let img = document.createElement('img');
+        let contentDescription = document.createElement('p');
+        let titleProduit = document.createElement('h2');
+
+        let prixProduit = document.createElement('p');
+        
 });
 });
