aa5dbf61a8ae2ac4ddc36ee9d557c1da44b77802
Adrizen89 <64806520+Adrizen89@users.noreply.github.com>
2020-09-17 14:51:50 +0200

    Create panier.js

---
+++ panier.js
@@ -0,0 +1,29 @@
+var maRequete = new Request('http://localhost:3000/api/furniture/');
+
+fetch(maRequete)
+.then(response => response.json())
+.then(response =>{
+    console.log(response)
+    
+                let mainPanier = document.getElementById('main-panier');
+
+                let listeProduits = document.createElement('section');
+                let produit = document.createElement('article');
+                let prixTotal = document.createElement('article');
+
+                let img = document.createElement('img');
+                let titleProduit = document.createElement('h3');
+                let prixProduit = document.createElement('p');
+                let annuler = document.createElement('button');
+
+
+                mainPanier.append(listeProduits);
+                listeProduits.append(produit);
+
+                produit.append(img);
+                produit.append(titleProduit);
+                produit.append(prixProduit);
+                produit.append(annuler);
+
+                
+});
