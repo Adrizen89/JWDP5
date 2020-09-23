59335c09f39842b0d57d39dcd5d8f0b13a4efdb0
Adrizen89 <64806520+Adrizen89@users.noreply.github.com>
2020-09-23 13:22:00 +0200

    Update panier.js

--- panier.js
+++ panier.js
@@ -11,26 +11,11 @@ fetch(maRequete)
 .then(response => response.json())
 .then(response =>{
     console.log(response)
-    
-                let mainPanier = document.getElementById('main-panier');
+    response.forEach(element => {
+        let main = document.getElementById('main-panier');
 
-                let listeProduits = document.createElement('section');
-                let produit = document.createElement('article');
-                let prixTotal = document.createElement('article');
-
-                let img = document.createElement('img');
-                let titleProduit = document.createElement('h3');
-                let prixProduit = document.createElement('p');
-                let annuler = document.createElement('button');
-
-
-                mainPanier.append(listeProduits);
-                listeProduits.append(produit);
-
-                produit.append(img);
-                produit.append(titleProduit);
-                produit.append(prixProduit);
-                produit.append(annuler);
-
+        localStorage.getItem("panier")
+        
+    });
                 
 });
