1239397e2cea136c30524bd8e18bb0a80062d04c
Adrizen89 <64806520+Adrizen89@users.noreply.github.com>
2020-10-06 15:53:14 +0200

    Update confirmation.js

--- confirmation.js
+++ confirmation.js
@@ -8,23 +8,19 @@ else{
     node.textContent = 0;
 }
 
-fetch("http://localhost:3000/api/furniture")
-            .then(response => response.json())
-            .then(response =>{
-                console.log(response);
+const orderId = JSON.parse(localStorage.getItem("orderId"));
+let main = document.getElementById('main-confirmation');
+                orderId.forEach(element => {
 
-                response.forEach(element => {
+                    
 
-                    const orderId = JSON.parse(localStorage.getItem("orderId"));
-
-                    let main = document.getElementById('main-confirmation');
+                    
                     let container = document.createElement('article');
                     main.append(container);
 
                     let id = document.createElement('p');
-                    id.textContent = response._id;
+                    id.textContent = element.orderId;
                     container.append(id);
                 });
 
-            });
 
