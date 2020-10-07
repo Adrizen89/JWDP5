9f561e22a54ccaddc79d6e074eaf3e0e8eae25aa
Adrizen89 <64806520+Adrizen89@users.noreply.github.com>
2020-10-07 22:28:58 +0200

    Update confirmation.js

--- confirmation.js
+++ confirmation.js
@@ -8,19 +8,45 @@ else{
     node.textContent = 0;
 }
 
-const orderId = JSON.parse(localStorage.getItem("orderId"));
-let main = document.getElementById('main-confirmation');
-                orderId.forEach(element => {
 
-                    
+let container = document.getElementById("main-confirmation");
 
-                    
-                    let container = document.createElement('article');
-                    main.append(container);
+let article = document.createElement('article');
+article.setAttribute("id", "article");
+container.append(article);
+//Affichage Identifiant de commande 
+let divId = document.createElement('div');
+divId.setAttribute("class", "identifiant");
+article.append(divId);
 
-                    let id = document.createElement('p');
-                    id.textContent = element.orderId;
-                    container.append(id);
-                });
+let textId = document.createElement('p');
+textId.textContent = "Votre identifiant est : ";
+divId.append(textId);
+
+let id = document.createElement('p');
+id.textContent = localStorage.getItem("orderId");
+id.setAttribute("class", "id");
+divId.append(id);
+
+//Affichage du Prix Total de la commande
+let divPrix = document.createElement('div');
+divPrix.setAttribute("class", "prixTotal");
+article.append(divPrix);
+
+let textPrix = document.createElement('p');
+textPrix.textContent = "Le prix total de votre commande est de :";
+divPrix.append(textPrix);
+
+let prixTotal = document.createElement('p');
+prixTotal.textContent = localStorage.getItem("total") / 1000 +"0€";
+prixTotal.setAttribute("class", "prix");
+divPrix.append(prixTotal);
+
+let merci = document.createElement('p');
+merci.textContent = "Orinoco vous remercie de votre commande !";
+merci.setAttribute("class", "merci");
+article.append(merci);
+
+                
 
 
